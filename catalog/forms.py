96afd6538cbd936eaae6561b1d496b6aa3c31440
Veeraja-Veeraesh <veeraja.v@gmail.com>
2025-01-08 15:59:54 +0530

    FInished part 9 mdn django

--- catalog/forms.py
+++ catalog/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
-class RenewBookForm(forms.form):
+class RenewBookForm(forms.Form):
     renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
 
     def clean_renewal_date(self):
